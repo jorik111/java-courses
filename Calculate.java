2f17afeb0c9c8bc43e0f4637b6b2466693735362
gora_mail <y1653101>
2018-08-04 13:41:53 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,15 @@ public class Calculate{
 
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
-
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int a = first + second; 
+		System.out.println("sum "+ a);
+		int b = first * second;
+		System.out.println("umnog "+ b); 
+		float c = first / second;
+		System.out.println("delen " + c);
+		int d = first - second;
+		System.out.println("raznic " + d);
 }
 }
